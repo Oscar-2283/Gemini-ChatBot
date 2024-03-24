e3494e6471e0f653a43693e1527539a17b042de4
Oscar <oscar50048@gmail.com>
2024-03-25 00:42:24 +0800

    (調整) main,tsx 改回 strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ChatRoom from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
+  <React.StrictMode>
     <ChatRoom />
-
+  </React.StrictMode>,
 )
